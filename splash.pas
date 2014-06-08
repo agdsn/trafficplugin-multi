1f8555741c87b783f61a62cfa6aaf02f93b60180
Tim Kluge <timklge@wh2.tu-dresden.de>
2014-06-08 15:19:06 +0200

    Use new icons

--- splash.pas
+++ splash.pas
@@ -1,7 +1,7 @@
 unit splash;
 
 {$mode objfpc}{$H+}
-// {$DEFINE debug}
+{$DEFINE debug}
 
 interface
 
@@ -11,6 +11,9 @@ uses
 
 const
   UsedApiVersion = 1;
+
+  IconActive = 22;
+  IconInActive = 0;
 
 type
   TTrafficStatus = (tsError, tsOK, tsCritical);
@@ -38,7 +41,7 @@ type
     procedure requesttimerTimer(Sender: TObject);
     procedure trafgetDoneInput(ASocket: TLHTTPClientSocket);
     procedure trafgetError(const msg: string; aSocket: TLSocket);
-    function trafgetInput(ASocket: TLHTTPClientSocket; ABuffer: pchar;
+    function trafgetInput(ASocket: TLHTTPClientSocket; ABuffer: PChar;
       ASize: integer): integer;
     procedure trayPaint(Sender: TObject);
   private
@@ -46,8 +49,8 @@ type
     lastPercentValue: single;
     status: TTrafficStatus;
 
-    procedure reportError(msg: String);
-    procedure reportMessage(msg: String);
+    procedure reportError(msg: string);
+    procedure reportMessage(msg: string);
     procedure processResult();
 
     procedure UpdateIcon;
@@ -83,7 +86,7 @@ end;
 
 procedure TSplashform.FormShow(Sender: TObject);
 begin
-  minimizetimer.Enabled := true;
+  minimizetimer.Enabled := True;
   requesttimerTimer(Sender);
 end;
 
@@ -103,7 +106,7 @@ begin
   Hide;
   lastPercentValue := 0;
 
-  minimizetimer.Enabled := false;
+  minimizetimer.Enabled := False;
 end;
 
 procedure TSplashform.requesttimerTimer(Sender: TObject);
@@ -123,8 +126,8 @@ begin
   reportError(msg);
 end;
 
-function TSplashform.trafgetInput(ASocket: TLHTTPClientSocket; ABuffer: pchar;
-  ASize: integer): integer;
+function TSplashform.trafgetInput(ASocket: TLHTTPClientSocket;
+  ABuffer: PChar; ASize: integer): integer;
 var
   oldLength: dword;
 begin
@@ -139,9 +142,10 @@ begin
 
 end;
 
-procedure TSplashform.reportError(msg: String);
+procedure TSplashform.reportError(msg: string);
 begin
-  statuslabel.caption := msg;
+  msg := 'Trafficstatus kann nicht angezeigt werden. ' + #13#10 + msg;
+  statuslabel.Caption := msg;
   statuslabel.Font.Color := clred;
   status := tsError;
   tray.Hint := msg;
@@ -149,9 +153,9 @@ begin
   Refresh;
 end;
 
-procedure TSplashform.reportMessage(msg: String);
+procedure TSplashform.reportMessage(msg: string);
 begin
-  statuslabel.caption := msg;
+  statuslabel.Caption := msg;
   statuslabel.font.color := clblack;
   status := tsOK;
   tray.Hint := msg;
@@ -161,51 +165,54 @@ end;
 
 procedure TSplashform.processResult;
 var
-  percent: Single;
+  percent: single;
 begin
-  if(UsedApiVersion = 1) then begin
-    if(HTTPBuffer = '-1') then begin
+  if (UsedApiVersion = 1) then
+  begin
+    if (HTTPBuffer = '-1') then
+    begin
       reportError('Nicht im AGDSN-Netz oder keine Verbindung möglich.');
       Exit;
     end;
     percent := StrToFloatDef(httpBuffer, -1);
-    if(percent < 0) then begin
+    if (percent < 0) then
+    begin
       reportError('Ungültige Antwort vom Server.');
       Exit;
     end;
 
-    if(percent > 40) and (lastPercentValue <= 40) then begin
-      tray.BalloonFlags:=bfWarning;
-      tray.BalloonTitle:='Hoher Trafficverbrauch';
-      tray.BalloonHint:='Du hast ' + FormatFloat('##0.##', percent) + ' % Traffic verbraucht. Achtung: Bei 100 % wirst du gesperrt!';
-      tray.BalloonTimeout:=10000;
+    if (percent > 40) and (lastPercentValue <= 40) then
+    begin
+      tray.BalloonFlags := bfWarning;
+      tray.BalloonTitle := 'Hoher Trafficverbrauch';
+      tray.BalloonHint := 'Du hast ' + FormatFloat('##0.##', percent) +
+        ' % Traffic verbraucht. Achtung: Bei 100 % wirst du gesperrt!';
+      tray.BalloonTimeout := 10000;
       tray.ShowBalloonHint;
     end;
 
     lastPercentValue := percent;
     reportMessage('Traffic zu ' + FormatFloat('##0.##', percent) + ' % aufgebraucht.');
+
+    ImageList1.GetIcon(1 + round(percent / 5), tray.Icon);
   end;
 end;
 
 procedure TSplashform.UpdateIcon;
 begin
   // Workaround: Keine Transparenz für Icon im Tray unter Linux
-  if(status = tsOK) then begin
-    {$IFNDEF Windows}
-    ImageList1.GetIcon(1, tray.Icon);
-    {$ELSE}
-    ImageList1.GetIcon(0, tray.Icon);
-    {$ENDIF}
-  end else if(status = tsCritical) then begin
+  if (status = tsOK) then
+  begin
+    ImageList1.GetIcon(IconActive, tray.Icon);
+  end
+  else if (status = tsCritical) then
+  begin
 
-  end else if(status = tsError) then begin
-    {$IFNDEF Windows}
-    ImageList1.GetIcon(3, tray.Icon);
-    {$ELSE}
-    ImageList1.GetIcon(2, tray.Icon);
-    {$ENDIF}
+  end
+  else if (status = tsError) then
+  begin
+    ImageList1.GetIcon(IconInActive, tray.Icon);
   end;
 end;
 
 end.
-
